3ed30cf748c7a4c7c239f9d098d1ff1c8f17cf8a
Guerra24 <pablo230699@hotmail.com>
2018-04-09 22:42:35 -0600

    Fixed crash

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const {app, Menu, Tray} = require('electron');
+const path = require('path');
+const {app, nativeImage, Menu, Tray} = require('electron');
 const express = require('./server.js'); // your express app
 
 // Electron
@@ -15,8 +16,9 @@ app.on('ready', () => {
 			}
 		},
 	]);
-
-	tray = new Tray('assets/icon-16.png');
+	const iconPath = path.join(__dirname, 'assets/icon-16.png');
+	const trayIcon = nativeImage.createFromPath(iconPath);
+	tray = new Tray(trayIcon);
 
 	tray.setToolTip('Firefox Discord Rich Presence');
 	tray.setContextMenu(contextMenu);
